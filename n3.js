932c693972c363bcc207d315aed8537ba8a34f28
NikitaKovyazov <92822645+NikitaKovyazov@users.noreply.github.com>
2022-05-24 15:34:05 +0300

    Create n3.js

---
+++ n3.js
@@ -0,0 +1,10 @@
+let calculateSalary = function (blackSalary) {
+const percent = 0.35
+if (blackSalary >= 100000) {
+percent = 0.45;
+}
+let tax = percent * blackSalary;
+let whiteSalary = blackSalary - tax;
+return whiteSalary;
+};
+console.log(calculateSalary(100000))
